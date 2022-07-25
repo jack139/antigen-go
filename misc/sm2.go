2859cb64cf27a61850954fca134c80fd1d4bb7c0
gt <jack139@gmail.com>
2022-07-25 21:36:23 +0800

    update sm2.go

--- misc/sm2.go
+++ misc/sm2.go
@@ -44,36 +44,30 @@ func main() {
 	sm3DigestData, _ := pubKey.Sm3Digest(data, nil)
 	fmt.Printf("Sm3Digest: %x\n", sm3DigestData)
 
-	sign, err := priv.Sign(rand.Reader, data, nil) // 签名(内部已经做了sm3摘要)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("sign: %x\n", sign)
-
-	R1, S1, _ := sm2.SignDataToSignDigit(sign) // 拆分出 R S
+	// 签名(内部已经做了sm3摘要)
+	R1, S1, _ := sm2.Sm2Sign(priv, data, nil, rand.Reader) 
 	fmt.Printf("R1: %x\nS1: %x\n", R1, S1)
 
-	sign1, _ := sm2.SignDigitToSignData(R1, S1)  // 重新合并 R S
-	fmt.Printf("sign1: %x\n", sign1)
-
-	ok := pubKey.Verify(data, sign1) // 密钥验证
+	// 验签
+	ok := sm2.Sm2Verify(&pubKey, data, nil, R1, S1) 
 	if ok != true {
 		log.Printf("Verify error\n")
 	} else {
 		log.Printf("Verify ok\n")
 	}
 
+
 	// python 生成的签名
-	signStr2 := "1a0a614198d2705c6248285737d01b0e4d66e285ec77fab91d8c9da0bd82a3e27e802a14e5322d75eaee7fbeecd8a93d71f94692577f1f9b89f0f0f63acfbefa"
-	//signStr2 := "6461b7f242019bf4ebb11f47ef43af9e25611918e04675ac30e94dd7424c5d59ee003fefbc2c4a7b65d8d1bbd5b6e60b05179c7cf6e5a0fbab630fc951106204"
-	R2, _ := new(big.Int).SetString(signStr2[:64], 16) // 拆分 R S
+	//signStr2 := "1a0a614198d2705c6248285737d01b0e4d66e285ec77fab91d8c9da0bd82a3e27e802a14e5322d75eaee7fbeecd8a93d71f94692577f1f9b89f0f0f63acfbefa"
+	signStr2 := "6461b7f242019bf4ebb11f47ef43af9e25611918e04675ac30e94dd7424c5d59ee003fefbc2c4a7b65d8d1bbd5b6e60b05179c7cf6e5a0fbab630fc951106204"
+
+	// 拆分 R S
+	R2, _ := new(big.Int).SetString(signStr2[:64], 16) 
 	S2, _ := new(big.Int).SetString(signStr2[64:], 16)
 	fmt.Printf("R2: %x\nS2: %x\n", R2, S2)
 
-	sign2, _ := sm2.SignDigitToSignData(R2, S2) // 合并 R S
-	fmt.Printf("sign2: %x\n", sign2)
-
-	ok = pubKey.Verify(data, sign2) // 密钥验证
+	// 验签
+	ok = sm2.Sm2Verify(&pubKey, data, nil, R2, S2)
 	if ok != true {
 		log.Printf("Verify error\n")
 	} else {
