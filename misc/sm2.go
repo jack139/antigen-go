3110816aeab5f8912f1585fe8fa89981e32ca517
gt <jack139@gmail.com>
2022-07-26 09:49:20 +0800

    update sm2.go

--- misc/sm2.go
+++ misc/sm2.go
@@ -6,54 +6,108 @@ import (
 	"log"
 	"math/big"
 	"encoding/base64"
+	"encoding/hex"
 	"github.com/tjfoc/gmsm/sm2"
 )
 
 
 var (
 	// 用户密钥
-	daPriStr = string("vK3iPBFMwKvXfS6QG3s0fKNPjGnLy90VI+PI0kzQ3o0=")
+	privBase64 = string("vK3iPBFMwKvXfS6QG3s0fKNPjGnLy90VI+PI0kzQ3o0=")
+
+	privKey *sm2.PrivateKey
+	pubKey *sm2.PublicKey
 )
 
+func init(){
+	// base64 恢复私钥
+	privKey, _ = restoreKey(privBase64)
+	//fmt.Printf("D: %x\nX: %x\nY: %x\n", priv.D, priv.PublicKey.X, priv.PublicKey.Y)
+
+	// 公钥
+	pubKey = &privKey.PublicKey
+}
 
 // 从 base64私钥 恢复密钥对
-func restoreKey(privStr string) *sm2.PrivateKey {
-	priv, _  := base64.StdEncoding.DecodeString(privStr)
-	fmt.Printf("priv %d %v\n", len(priv), priv)
+func restoreKey(privStr string) (*sm2.PrivateKey, error) {
+	priv, err  := base64.StdEncoding.DecodeString(privStr)
+	if err!=nil {
+		return nil, err
+	}
 
 	curve := sm2.P256Sm2()
 	key := new(sm2.PrivateKey)
 	key.PublicKey.Curve = curve
 	key.D = new(big.Int).SetBytes(priv)
 	key.PublicKey.X, key.PublicKey.Y = curve.ScalarBaseMult(priv)
-	return key
+	return key, nil
+}
+
+// SM2签名
+func SM2Sign(data []byte) ([]byte, error) {
+	// 签名(内部已经做了sm3摘要)
+	R, S, err := sm2.Sm2Sign(privKey, data, nil, rand.Reader) 
+	if err!=nil {
+		return nil, err
+	}
+	//fmt.Printf("R: %x\nS: %x\n", R, S)
+
+	sign := R.Bytes()
+	sign = append(sign, S.Bytes()...)
+
+	return sign, nil
+}
+
+// SM2签名，返回base64编码
+func SM2SignBase64(data []byte) (string, error) {
+	sign, err := SM2Sign(data)
+	if err!=nil {
+		return "", err
+	}
+
+	return base64.StdEncoding.EncodeToString(sign), nil
+}
+
+// SM2验签
+func SM2Verify(data []byte, sign []byte) bool {
+	if len(sign)<64 {
+		return false
+	}
+
+	R := new(big.Int).SetBytes(sign[:32]) 
+	S := new(big.Int).SetBytes(sign[32:])
+	//fmt.Printf("R: %x\nS: %x\n", R, S)
+
+	// 验签
+	return sm2.Sm2Verify(pubKey, data, nil, R, S)
+}
+
+// SM2验签，使用base64编码
+func SM2VerifyBase64(data []byte, signBase64 string) bool {
+	sign, _  := base64.StdEncoding.DecodeString(signBase64)
+	return SM2Verify(data, sign)
 }
 
 
 func main() {
 	data := []byte("message digest")
 
-	// base64 恢复私钥
-	priv := restoreKey(daPriStr)
-	fmt.Printf("D: %x\nX: %x\nY: %x\n", priv.D, priv.PublicKey.X, priv.PublicKey.Y)
+	sign, _ := SM2Sign(data)
+	fmt.Printf("sign: %x\n", sign)
 
-	// 公钥
-	pubKey := priv.PublicKey
-
-	// 验证sm3摘要结果
-	sm3DigestData, _ := pubKey.Sm3Digest(data, nil)
-	fmt.Printf("Sm3Digest: %x\n", sm3DigestData)
-
-	// 签名(内部已经做了sm3摘要)
-	R1, S1, _ := sm2.Sm2Sign(priv, data, nil, rand.Reader) 
-	fmt.Printf("R1: %x\nS1: %x\n", R1, S1)
-
-	// 验签
-	ok := sm2.Sm2Verify(&pubKey, data, nil, R1, S1) 
+	ok := SM2Verify(data, sign)
 	if ok != true {
-		log.Printf("Verify error\n")
+		log.Printf("SM2Verify() Verify error\n")
 	} else {
-		log.Printf("Verify ok\n")
+		log.Printf("SM2Verify() Verify ok\n")
+	}
+
+	signBase64, _ := SM2SignBase64(data)
+	ok = SM2VerifyBase64(data, signBase64)
+	if ok != true {
+		log.Printf("SM2VerifyBase64() Verify error\n")
+	} else {
+		log.Printf("SM2VerifyBase64() Verify ok\n")
 	}
 
 
@@ -61,17 +115,23 @@ func main() {
 	//signStr2 := "1a0a614198d2705c6248285737d01b0e4d66e285ec77fab91d8c9da0bd82a3e27e802a14e5322d75eaee7fbeecd8a93d71f94692577f1f9b89f0f0f63acfbefa"
 	signStr2 := "6461b7f242019bf4ebb11f47ef43af9e25611918e04675ac30e94dd7424c5d59ee003fefbc2c4a7b65d8d1bbd5b6e60b05179c7cf6e5a0fbab630fc951106204"
 
-	// 拆分 R S
-	R2, _ := new(big.Int).SetString(signStr2[:64], 16) 
-	S2, _ := new(big.Int).SetString(signStr2[64:], 16)
-	fmt.Printf("R2: %x\nS2: %x\n", R2, S2)
+	fmt.Println("python signed: ", signStr2)
 
 	// 验签
-	ok = sm2.Sm2Verify(&pubKey, data, nil, R2, S2)
+	sign, _ = hex.DecodeString(signStr2)
+	ok = SM2Verify(data, sign)
 	if ok != true {
-		log.Printf("Verify error\n")
+		log.Printf("SM2Verify() Verify error\n")
 	} else {
-		log.Printf("Verify ok\n")
+		log.Printf("SM2Verify() Verify ok\n")
+	}
+
+	signBase64 = base64.StdEncoding.EncodeToString(sign)
+	ok = SM2VerifyBase64(data, signBase64)
+	if ok != true {
+		log.Printf("SM2VerifyBase64() Verify error\n")
+	} else {
+		log.Printf("SM2VerifyBase64() Verify ok\n")
 	}
 
 }
